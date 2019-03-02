ef260358abb6c1975ee9ae67021449146f78fdb4
Dylan Schirino <dylan.schirino@djmweb.be>
2019-03-02 11:35:54 +0100

    Add routing & component base creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TownComponent } from './town/town.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TownComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
